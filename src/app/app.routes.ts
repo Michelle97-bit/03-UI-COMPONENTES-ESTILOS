6ea0fcb4e133a3093134803a32b0bd324461fb62
srodriguuezz1 <stevenrod1996@hotmail.com>
2025-11-23 12:08:32 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EstilosPage } from './features/estilos-page/estilos-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: EstilosPage },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/features/estilos-page/estilos-page.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SignalBoxComponent } from '../signal-box/signal-box';
+
+@Component({
+  selector: 'app-estilos-page',
+  standalone: true,
+  templateUrl: './estilos-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [SignalBoxComponent]
+})
+export class EstilosPage {}
